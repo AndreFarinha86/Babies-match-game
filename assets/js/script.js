9903618cf8cb89ea135d773555231aabfbd064e7
Andre Farinha <andre.f.a.farinha@gmail.com>
2023-02-21 16:26:18 +0000

    Update Modal JavaScript, HTML and CSS strcture for Play and Rules Button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
-let playBtn = document.getElementsByClassName("btn-play");
-let modalPlay = document.getElementsByClassName("modal-play");
+// -- MODALS RULES --
 
-playBtn.addEventListener("click", function(){
-    modalPlay.classList.add("active");
+// Get the button that opens the modal
+let modalOpen = document.querySelectorAll(".modal-open");
+
+// When the user clicks the button, open the modal 
+modalOpen.forEach(function(btn){
+    btn.onclick = function() {
+        let modal = btn.getAttribute("data-modals");
+
+        document.getElementById(modal).style.display = "block";
+    };
 });
+
+// Get the button  that closes the modal
+let modalClose = document.querySelectorAll(".modal-close");
+
+// When the user clicks on Button (x), close the modal
+modalClose.forEach(function(btn){
+    btn.onclick = function() {
+        let modal = (btn.closest(".modals").style.display="none");
+    };
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+   if (event.target.className === "modals" ) {
+    event.target.style.display = "none";
+   }
+  };
